471387d5960b53f24b655bc917fe3ad84355e13d
Sahana Acharya <agsahana11@gmail.com>
2023-12-02 08:29:47 +0530

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //url: "mongodb://localhost:27017/bezkoder_db"
+  url:"mongodb://testsahana:84BwYbdhgEz3k3oUZL0rgFwMgGZGUdPSDa0oGxwqXCtBLT3jHejAIUYREfnveUkxBq9W8R6JfJGHACDbJoaf0w==@testsahana.mongo.cosmos.azure.com:10255/testsahana?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@testsahana@"
+};
